a19b6ae8996603a7982365372fc0cf9406a6f7e2
topcat <topcattw@gmail.com>
2017-07-03 10:50:10 +0800

    Sample for controller Cookie get & set
    Sample for controller Cookie get & set

---
+++ Controllers/ValuesController.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Net
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Web.Http
+
+Public Class ValuesController
+    Inherits ApiController
+
+    ' GET api/values
+    Public Function GetValues() As IEnumerable(Of String)
+        Dim utilCookie As New CookieUtil
+
+        '取得Cookie
+        Dim getUToken As String = utilCookie.getCookie(Request, "UToken")
+
+        '設定Cookie
+        'Dim Cookie As HttpCookie = New HttpCookie("UToken", "UToken" & Format(Now, "yyyyMMddHHmmss"))
+        'Cookie.Expires = DateAdd(DateInterval.Minute, 20, Now())
+        'HttpContext.Current.Response.Cookies.Add(Cookie)
+        Dim CookieValue As String = "UToken" & Format(Now, "yyyyMMddHHmmss")
+        Dim CookieExpires As DateTime = DateAdd(DateInterval.Minute, 20, Now())
+
+        utilCookie.setCookie("UToken", CookieValue, CookieExpires, "", "")
+
+        Return New String() {"value1", "value2", getUToken}
+    End Function
+
+    ''' <summary>
+    ''' 取得Cookie
+    ''' </summary>
+    ''' <param name="request">Request</param>
+    ''' <param name="CookieName">Cookie的名字</param>
+    ''' <returns></returns>
+    Public Function getCookie(ByVal request As HttpRequestMessage, ByVal CookieName As String) As String
+        Dim Rc As String = ""
+        Dim cookie As CookieHeaderValue = request.Headers.GetCookies(CookieName).FirstOrDefault()
+        If cookie IsNot Nothing Then
+            Rc = cookie(CookieName).Value
+        End If
+        Return Rc
+    End Function
+
+    '' GET api/values/5
+    'Public Function GetValue(ByVal id As Integer) As String
+    '    Return "value"
+    'End Function
+
+    '' POST api/values
+    'Public Sub PostValue(<FromBody()> ByVal value As String)
+
+    'End Sub
+
+    '' PUT api/values/5
+    'Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+    'End Sub
+
+    '' DELETE api/values/5
+    'Public Sub DeleteValue(ByVal id As Integer)
+
+    'End Sub
+End Class
---
+++ Models/Utility/CookieUtil.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Headers
+
+Public Class CookieUtil
+
+    ''' <summary>
+    ''' 取得Cookie
+    ''' </summary>
+    ''' <param name="request">Request</param>
+    ''' <param name="CookieName">Cookie的名字</param>
+    ''' <returns></returns>
+    Public Function getCookie(ByVal request As HttpRequestMessage, ByVal CookieName As String) As String
+        Dim Rc As String = ""
+        Dim cookie As CookieHeaderValue = request.Headers.GetCookies(CookieName).FirstOrDefault()
+        If cookie IsNot Nothing Then
+            Rc = cookie(CookieName).Value
+        End If
+        Return Rc
+    End Function
+
+    Public Sub setCookie(ByVal CookieName As String, ByVal CookieValue As String, ByVal CookieExpires As DateTime, ByVal CookieDomain As String, ByVal CookiePath As String)
+        '設定Cookie
+        Dim Cookie As HttpCookie = New HttpCookie("UToken", "UToken" & Format(Now, "yyyyMMddHHmmss"))
+        'Cookie.Expires = DateAdd(DateInterval.Minute, 20, Now())
+        With Cookie
+            .Expires = CookieExpires
+            If CookieDomain <> "" Then
+                .Domain = CookieDomain
+            End If
+            If CookiePath <> "" Then
+                .Path = CookiePath
+            End If
+        End With
+        HttpContext.Current.Response.Cookies.Add(Cookie)
+    End Sub
+
+
+End Class
